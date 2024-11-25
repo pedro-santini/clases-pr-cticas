7f0657f855836536e06caec108f373c1e8a8220f
Pedro Santini <psantini96@gmail.com>
2024-11-24 21:03:22 -0300

    Javascript_Práctica Clase 1

---
+++ js/java.js
@@ -0,0 +1,17 @@
+let nombre;
+nombre = prompt("Ingrese su nombre");
+
+let edad;
+edad = parseInt(prompt("Ingrese su edad ")); 
+
+let boolean;
+if (edad>=18) {
+    boolean=true
+} else {
+    boolean=false
+    
+};
+
+console.log(nombre);
+console.log(edad);
+console.log(boolean);
